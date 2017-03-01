801e7a85bf1aac8b706e92d6f1c7046de42071fa
Pascal Burkhardt <pascalburkhardt@mail.de>
2017-03-01 14:26:54 +0100

    Add Sample Files for SonarQube Test

---
+++ example-project/src/main/java/my/pack/Greeting.java
@@ -0,0 +1,14 @@
+package my.pack;
+
+public class Greeting {
+    public void coveredByTest() {
+       System.out.println("Hello, world.");
+    }
+
+	public boolean foobar() {
+		// 2 findbugs issues:
+		// findbugs:DM_NUMBER_CTOR
+		// findbugs:EC_UNRELATED_TYPES
+		return new Integer(0).equals("foo");
+	}
+}
---
+++ example-project/src/main/java/my/pack/XtendGreeting.xtend
@@ -0,0 +1,15 @@
+package my.pack
+
+class XtendGreeting {
+
+    def greeting() {
+        "Hello, world."
+    }
+
+    def useEquals() {
+        // 2 findbugs issues:
+        // findbugs:DM_NUMBER_CTOR
+        // findbugs:EC_UNRELATED_TYPES
+        new Integer(0).equals("foo");
+    }
+}
